f04cf6aaaa84425cc0436c2a607f9e0b715b3982
Julia Almeida <julia.almeida@g.globo>
2025-04-04 14:37:04 -0300

    feat: cep meu e cep do professor, para testes de pandas, tqdm e csv

--- aula09/cep.py
+++ aula09/cep.py
@@ -1,23 +1,29 @@
 # %%
 import requests
 import json
+import pandas as pd
 
+endereco = input("Digite seu CEP (somente números): ")
 
-endereco = input("Digite seu CEP: ")
-
-url = f"https://viacep.com.br/ws/{endereco}/json/" 
+url = f"https://viacep.com.br/ws/{endereco}/json/"
 resposta = requests.get(url)
-
 
 if resposta.status_code == 200:
     dados = resposta.json()
-    print("Seu CEP é:", dados)
 
-    with open("ceps.json", "w", encoding="utf-8") as open_file:
-        json.dump(dados, open_file, ensure_ascii=False, indent=4)
+    if "erro" in dados:
+        print("CEP inválido! Tente novamente.")
+    else:
+        print("Seu CEP é:", dados)
 
+        with open("ceps.json", "w", encoding="utf-8") as open_file:
+            json.dump(dados, open_file, ensure_ascii=False, indent=4)
+
+        dataset = pd.DataFrame([dados])
+        print(dataset)
 else:
-    print("CEP invalido!, tente novamente")
+    print("Erro ao conectar com o serviço. Tente novamente mais tarde.")
+
 
 #### aula 12 - dados api (30:30) 
 
